e4c2e97083bb665ea9b28f2f8d75c6d87eee5032
Zebbly <zachschuller97@gmail.com>
2018-02-18 11:09:22 -0600

    Add files via upload
    This version has a completely revamped structure to incorporate running epi2me processes from it and gathering telemetry files on its own

--- telparser.py
+++ telparser.py
@@ -5,11 +5,13 @@ import argparse
 def argumentGet():
     parser = argparse.ArgumentParser(description='Run estimators and gather data from MINion telemetry files')
     parser.add_argument('-a', '--auto', help='turns run off automatically if tests are within acceptable parameters', action='store_true')
-    parser.add_argument('location', help='location of the telemetry file')
+    parser.add_argument('-l', '--location', help='location of the telemetry file', default=None)
     parser.add_argument('-%', '--percent', type=float, help='accuracy percentage threshold', default=70.0)
     parser.add_argument('-t', '--time', type=int, default=30, help='time between checks of file')
     parser.add_argument('runID', help='unique identifier for this run')
     parser.add_argument('-v', '--verbose', help='prints output to console', action='store_true')
+    parser.add_argument('-i', '--input', help="input folder for barcoding to 16s run", default=None)
+    parser.add_argument('-b', '--barcode', help="number of barcoders used", default=None)
     args = parser.parse_args()
     return args
 
